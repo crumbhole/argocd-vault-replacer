14591ef07472f0e5480375898105b2e4cac3cddb
Alan Clucas <alan.clucas@worldprogramming.com>
2021-02-18 09:52:09 +0000

    Allow base64 encoded secrets to be substituted too

--- src/substitution/substitute.go
+++ src/substitution/substitute.go
@@ -1,12 +1,34 @@
 package substitution
 
 import (
+	"encoding/base64"
 	"regexp"
 )
 
+func substituteb64(input []byte) []byte {
+	decoded, err := base64.StdEncoding.DecodeString(string(input))
+	if err != nil {
+		return input
+	}
+	// This reValue differs from the one in Substitute as it requires a match on
+	// the whole thing
+	reValue := regexp.MustCompile(`^(<[ \t]*vault:[^\r\n]+?)>$`)
+	// We want to stuff |base64 on the end of this
+	matchFound := reValue.FindSubmatch(decoded)
+	if matchFound != nil {
+		return []byte(string(matchFound[1]) + `|base64>`)
+	}
+	return input
+}
+
 // Takes a whole multi-line []byte and finds appropriate subsitutions
 func Substitute(input []byte, source ValueSource) ([]byte, error) {
+	// First attempt to base64 decode any <vault:> secrets encoded by other
+	// tools, such as helm
+	reB64Value := regexp.MustCompile(`PHZhdWx0O[^\s]*`)
+	debase64input := reB64Value.ReplaceAllFunc(input, substituteb64)
+
 	reValue := regexp.MustCompile(`<[ \t]*vault:[^\r\n]+?>`)
 	subst := Substitutor{source: source}
-	return reValue.ReplaceAllFunc(input, subst.substituteValue), subst.errs
+	return reValue.ReplaceAllFunc(debase64input, subst.substituteValue), subst.errs
 }
--- src/substitution/substitute_test.go
+++ src/substitution/substitute_test.go
@@ -35,3 +35,39 @@ func TestStringSubst(t *testing.T) {
 		}
 	}
 }
+
+func TestB64Decode(t *testing.T) {
+	tests := map[string]string{
+		`Hello`:                                    `Hello`,
+		`<vault:/path/to/thing~foo>`:               `<vault:/path/to/thing~foo>`,
+		`PHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4=`:     `<vault:/path/to/thing~foo|base64>`,
+		`PHZhdWx0Oi9wYXRoL3RvL3RoaW5nIH4gZm9vID4=`: `<vault:/path/to/thing ~ foo |base64>`,
+		`a PHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4= b`: `a PHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4= b`,
+		`aPHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4=b`:   `aPHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4=b`,
+	}
+	for input, expect := range tests {
+		in := []byte(input)
+		res := substituteb64(in)
+		if !bytes.Equal(res, []byte(expect)) {
+			t.Errorf("%s !-> %v, got %s", in, expect, res)
+		}
+	}
+}
+
+func TestStringSubstB64(t *testing.T) {
+	tests := map[string]string{
+		`foo PHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4= to be here`: `foo YmFy to be here`,
+		`fooPHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4= to be here`:  `fooYmFy to be here`,
+		`fooPHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4=to be here`:   `fooPHZhdWx0Oi9wYXRoL3RvL3RoaW5nfmZvbz4=to be here`,
+	}
+	for input, expect := range tests {
+		in := []byte(input)
+		res, errs := Substitute(in, subst_vs)
+		if errs != nil {
+			t.Errorf("Got unexpected errors in substitute test %s", errs)
+		}
+		if !bytes.Equal(res, []byte(expect)) {
+			t.Errorf("%s !-> %v, got %s", in, expect, res)
+		}
+	}
+}
